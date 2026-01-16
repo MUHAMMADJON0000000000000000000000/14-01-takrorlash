5924117010e479f3b21b07edea203e8fc80300e6
Muhammadjon coding <86595049+MUHAMMADJON0000000000000000000000@users.noreply.github.com>
2026-01-16 10:36:24 +0500

    dwq

---
+++ index.js
@@ -0,0 +1,58 @@
+// function salomBer(ism) {
+//   return "Salom " + ism;
+// }
+// console.log(salomBer("Ali"));
+
+ 
+
+// function sogliqDarajasi(yosh, ogirlik) {
+//   if (yosh < 30 && ogirlik >= 60 && ogirlik <= 80) {
+//     return "Yaxshi sog'lom";
+//   } else if (yosh < 50 && ogirlik >= 55 && ogirlik <= 90) {
+//     return "O'rtacha";
+//   } else {
+//     return "Yomon";
+//   }
+// }
+
+// console.log(sogliqDarajasi(25, 70));
+  
+
+// const bankAccount = {
+//   balance: 0,
+
+//   deposit(summa) {
+//     this.balance += summa;
+//     console.log(summa + " so'm qo'shildi");
+//   },
+
+//   withdraw(summa) {
+//     if (summa > this.balance) {
+//       console.log("Hisobda yetarli mablag' yo'q");
+//     } else {
+//       this.balance -= summa;
+//       console.log(summa + " so'm yechildi");
+//     }
+//   },
+
+//   showBalance() {
+//     console.log("Balans: " + this.balance + " so'm");
+//   }
+// };
+
+// // sinab ko‘ramiz
+// bankAccount.deposit(500);
+// bankAccount.withdraw(200);
+// bankAccount.showBalance();
+
+
+// function Animal(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// const animal1 = new Animal("Tiger", 23);
+// const animal2 = new Animal("Lion", 15);
+
+// console.log(animal1);
+// console.log(animal2);
